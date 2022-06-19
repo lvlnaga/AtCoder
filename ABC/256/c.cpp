496589b552094a14e9729daffedcdcfbe2c1dbef
lVlnaga <cid15135@gmail.com>
2022-06-19 18:27:47 +0900

    abc256 cの復習

--- ABC/256/c.cpp
+++ ABC/256/c.cpp
@@ -10,74 +10,50 @@ using Graph = vector<vector<int>>;
 using mint = modint1000000007;
 
 int main() {
-  int h[4],w[4];
-  int a[4][4]= {0};
-  int h1[31][4]= {0},h2[31][4]= {0},h3[31][4]= {0},w1[31][4]= {0},w2[31][4]= {0},w3[31][4]= {0};
-  int tmp =0;
-  for (int i = 1; i <= 3; i++)
+  int h[3],w[3];
+  //int a[4][4]= {0};
+  vector<vector<int>> a(3,vector<int>(3)); //vector型で3x3の2次元配列を用意。
+
+  for (int i = 0; i < 3; i++)
   {
     cin >> h[i];
   }
-  for (int i = 1; i <= 3; i++)
+  for (int i = 0; i < 3; i++)
   {
     cin >> w[i];
   }
-  int tmp1=0;
-  for (int i = 1; i < h[1]; i++)
-  {
-    for (int j = 1; j < h[1] - i; j++)
-    {
-      if((h[1] - i - j) > 0){
-        h1[tmp1][1] = i;
-        h1[tmp1][2] = j;
-        h1[tmp1][3] = h[1] - i -j;
-        tmp1++;
-      }
-    }
-  }
-  int tmp2=0;
-  for (int i = 1; i < h[2]; i++)
-  {
-    for (int j = 1; j < h[2] - i; j++)
-    {
-      if((h[2] - i - j) > 0){
-        h2[tmp2][1] = i;
-        h2[tmp2][2] = j;
-        h2[tmp2][3] = h[2] - i -j;
-        tmp2++;
-      }
-    }
-  }
-  int tmp3=0;
-  for (int i = 1; i < h[3]; i++)
-  {
-    for (int j = 1; j < h[3] - i; j++)
-    {
-      if((h[3] - i - j) > 0){
-        h3[tmp3][1] = i;
-        h3[tmp3][2] = j;
-        h3[tmp3][3] = h[3] - i -j;
-        tmp3++;
-      }
-    }
-  }
-  
+
+  int m = 30;
+  int m4 = m*m*m*m;
   int ans = 0;
 
-  for (int i = 0; i < tmp1; i++)
+  for (int s = 0; s < m4; s++)
   {
-    for (int j = 0; j < tmp2; j++)
-    {
-      for (int k = 0; k < tmp3; k++)
-      {
-        if(h1[i][1] + h2[j][1] + h3[k][1] == w[1] &&
-            h1[i][2] + h2[j][2] + h3[k][2] == w[2] &&
-              h1[i][3] + h2[j][3] + h3[k][3] == w[3])
-                ans ++;
-      }
-      
-    }
+    int tmp = s;
+    a[0][0] = (tmp % m)+1; tmp/=m; //m進数として考える。1桁目を切り出して、1桁シフト。
+    a[0][1] = (tmp % m)+1; tmp/=m; //m進数として考える。1桁目を切り出して、1桁シフト。
+    a[1][0] = (tmp % m)+1; tmp/=m; //m進数として考える。1桁目を切り出して、1桁シフト。
+    a[1][1] = (tmp % m)+1; tmp/=m; //m進数として考える。1桁目を切り出して、1桁シフト。
+
+    bool ok = true; //条件満たしているかフラグ
     
+    //hのチェック
+    for (int i = 0; i < 2; i++)
+    {
+      a[i][2] = h[i] - a[i][0] - a[i][1];
+      if(a[i][2] < 1) ok = false;
+    }
+    //wのチェック
+    for (int i = 0; i < 3; i++)
+    {
+      a[2][i] = w[i] - a[0][i] - a[1][i];
+      if(a[2][i] < 1) ok = false;
+    }
+
+    //最後にh2もチェック
+    if((a[2][0] + a[2][1] + a[2][2]) != h[2]) ok = false;
+
+    if(ok) ans++;
   }
   
   cout << ans << endl;
