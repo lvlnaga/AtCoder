a261569300804a000ed7a7860bf117213fc08026
lVlnaga <cid15135@gmail.com>
2022-06-26 23:10:39 +0900

    253 b クリア

--- ABC/253/b.cpp
+++ ABC/253/b.cpp
@@ -10,10 +10,31 @@ using Graph = vector<vector<int>>;
 using mint = modint1000000007;
 
 int main() {
-  int n;
-  cin >> n;
+  int w,h;
+  cin >> h >> w;
+  vector <string> line(h);
+  char target = 'o';
+  int now = 0;
+  int gyo[2],retsu[2];
+  
+  string tmp = "null";
+
+  for (int i = 0; i < h; i++)
+  {
+    cin >> line[i];
+    for (int j = 0; j < w; j++)
+    {
+      if(target == (char)line[i][j]){
+        gyo[now] = i;
+        retsu[now] = j;
+        now ++;
+      }
+    }
+  }
 
   int ans = 0;
+
+  ans = abs(gyo[0]-gyo[1]) + abs(retsu[0]-retsu[1]);
 
   cout << ans << endl;
   return 0;
