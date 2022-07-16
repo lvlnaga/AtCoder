11a3dbc372839860c43afd300a1aad1cf766ea0f
lVlnaga <cid15135@gmail.com>
2022-07-16 19:33:33 +0900

    259 a clear

---
+++ ABC/259/a.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+#include <atcoder/all>
+using namespace atcoder;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep1(i, n) for (int i = 1; i < (int)(n+1); i++)
+using ll = long long;
+using P = pair<int,int>;
+using Graph = vector<vector<int>>;
+using mint = modint1000000007;
+
+int main() {
+  int n,m,x,t,d;
+  cin >> n >> m >> x >> t >> d;
+
+  int ans = 0;
+
+  if (x <= m && m <= n)
+  {
+    ans = t;
+  }
+  else if(m < x)
+  {
+    ans = t - ((x - m) * d);
+  }else{
+    ans = t - ((n - m) * d);
+  }
+  
+
+  cout << ans << endl;
+  return 0;
+}
