9c9680ad6f40caf9d7137f580e1dbd87d4fcc4e4
lVlnaga <cid15135@gmail.com>
2022-08-06 19:39:30 +0900

    266 a clear

--- ABC/259/c.cpp
+++ ABC/259/c.cpp
@@ -9,12 +9,48 @@ using P = pair<int,int>;
 using Graph = vector<vector<int>>;
 using mint = modint1000000007;
 
+vector<pair<char,int>> encode(const string& str){
+  int n = (int)str.size();
+  vector<pair<char,int>> ret;
+  for(int l = 0; l < n;){
+    int r = l + 1;
+    for ( ; r < n && str[l] == str[r]; r++){}
+    ret.push_back({str[l],r-l});
+    l = r;
+  }
+  return ret;
+}
+
+
+bool solve(){
+  string s, t;
+  cin >> s >> t;
+  auto a = encode(s);
+  auto b = encode(t);
+
+  if(a.size()!= b.size()) return false;
+  
+  for (int i = 0; i < (int)a.size(); i++)
+  {
+    if (a[i].first != b[i].first) return false;
+    int al = a[i].second;
+    int bl = b[i].second;
+
+    if(al == 1){
+      if(bl != 1) return false;
+    }else{
+      if(bl < al) return false;
+    }
+  }
+  
+
+  return true;
+}
+
+
 int main() {
-  int n;
-  cin >> n;
-
-  int ans = 0;
-
-  cout << ans << endl;
+  
+  if(solve()) cout << "Yes" << endl;
+  else cout << "No" << endl;
   return 0;
 }
---
+++ ABC/266/a.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+#include <atcoder/all>
+using namespace atcoder;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep1(i, n) for (int i = 1; i < (int)(n+1); i++)
+using ll = long long;
+using P = pair<int,int>;
+using Graph = vector<vector<int>>;
+using mint = modint1000000007;
+
+int main() {
+  int y;
+  cin >> y;
+
+  for (int i = y; i <= 3004 ; i++)
+  {
+    if ((i % 4) == 2)
+    {
+      cout << i << endl;
+      break;
+    }
+  }
+  
+
+  // int ans = 0;
+
+  // cout << ans << endl;
+  return 0;
+}
