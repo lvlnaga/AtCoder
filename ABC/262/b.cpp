33e3b0507b933c1c3cf1928c676c5b8932288b47
lVlnaga <cid15135@gmail.com>
2022-08-07 11:45:56 +0900

    modified:   ABC/262/b.cpp modified:   ABC/262/c.cpp modified:   problem.exe

--- ABC/262/b.cpp
+++ ABC/262/b.cpp
@@ -11,20 +11,36 @@ using mint = modint1000000007;
 
 int main() {
   int n,m;
-  
-  vector<pair<int,int>> u_v;
-  
   cin >> n >> m;
+  vector<vector<bool>> graph(n, vector<bool>(n));
 
+  //無向グラフを隣接行列で管理
+  //計算量は O(n^2 + m)
   for (int i = 0; i < m; i++)
   {
-    cout << "a" << endl;
+    int u,v;
+    cin >> u >> v;
+    u -= 1; v -= 1; //添字を0から始めるので頂点もそれに合わせて-1して扱う
+    graph[u][v] = graph[v][u] = true;
   }
   
 
-
+  //三角形があるか探す
+  //計算量はO(N^3)
   int ans = 0;
-
+  for (int i = 0; i < n; i++)
+  {
+    for (int j = i+1 ; j < n; j++)
+    {
+      for (int k = j + 1; k < n; k++)
+      {
+        if(graph[i][j] and graph[j][k] and graph[k][i]){
+          ans ++;
+        }
+      }
+    }
+  }
   cout << ans << endl;
+
   return 0;
 }
--- ABC/262/c.cpp
+++ ABC/262/c.cpp
@@ -11,10 +11,37 @@ using mint = modint1000000007;
 
 int main() {
   int n;
+  ll same,ans;
+
   cin >> n;
+  vector<int> a(n);
+  
+  ans = same = 0;
 
-  int ans = 0;
 
+  //a[i] == i になっているやつをカウント
+  for (int i = 0; i < n; i++)
+  {
+    cin >> a[i];
+    a[i] -= 1;
+
+    if(a[i] == i) same++;
+  }
+
+  //sameの中から2つを選んだやつは条件満たす。nC2を計算
+  ans = (same * (same - 1 ))/2;
+
+
+  //ひっくり返るパターンもありうる
+  //a[i] == j も探す → a[a[i]] == i を探せばOK.
+  //ひっくり返ってないとだめだから、 a[i] > iは大きいという条件もあり。
+  
+  for (int i = 0; i < n; i++)
+  {
+    if ((a[a[i]] == i) and (a[i] > i))
+      ans ++;
+  }
+  
   cout << ans << endl;
   return 0;
 }
