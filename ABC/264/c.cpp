4e7b590890e39dd4282fdaaf6c777cc6fe01dcee
lVlnaga <cid15135@gmail.com>
2022-08-15 07:29:18 +0900

    modified:   ABC/264/c.cpp 	modified:   ABC/264/h.cpp

--- ABC/264/c.cpp
+++ ABC/264/c.cpp
@@ -10,11 +10,82 @@ using Graph = vector<vector<int>>;
 using mint = modint1000000007;
 
 int main() {
-  int n;
-  cin >> n;
+  int h1,w1,h2,w2;
+  cin >> h1 >> w1;
 
-  int ans = 0;
+  //aの取り込み
+  vector<vector<int>> a(h1,vector<int>(w1));
+  for (int i = 0; i < h1; i++)
+  {
+    for (int j = 0; j < w1; j++)
+    {
+      cin >> a[i][j];
+    }
+  }
+  
 
-  cout << ans << endl;
+  //bの取り込み
+  cin >> h2 >> w2;
+  vector<vector<int>> b(h2,vector<int>(w2));
+  
+  for (int i = 0; i < h2; i++)
+  {
+    for (int j = 0; j < w2; j++)
+    {
+      cin >> b[i][j];
+    }
+  }
+
+  
+  for (int i = 0; i < (1<<h1); i++) //行のbit全探索
+  {
+    for (int j = 0; j < (1<<w1); j++)//列のbit全探索
+    {
+      
+      vector<int> hvec,wvec;
+      for (int k = 0; k < h1; k++) //何行目を残しておくかhvecへ保存
+      {
+        if((i & (1 << k)) == 0)
+          hvec.push_back(k);
+      }
+      
+      for (int k = 0; k < w1; k++)//何列目を残しておくかhvecへ保存
+      {
+        if((j & (1 << k)) == 0)
+          wvec.push_back(k);
+      }
+
+      //ここ何やっているのか不明。 size が違ったら続ける。はわかるけど、
+      //そうじゃなかったら続けない。っていうのは書かなくてよいの？
+      //<continue文とは>
+      //continueはforとかwhileのループの中で処理をスキップさせるために用いる
+      //if文と併用され、ループ処理の中である条件が成立する場合それ以下の処理を行わずにスキップする。
+      //じゃあskip文とかのほうがしっくり来る。
+      //<つまりここでは？>
+      //選択した行、列のサイズがターゲットのサイズと一致してなければ以降のマッチ確認は不要だよ。の意味
+      if( (int)hvec.size() != h2 || (int)wvec.size() != w2) continue; 
+  
+      bool match = true;
+
+      for (int k = 0; k < h2; k++)
+      {
+        for (int l = 0; l < w2; l++)
+        {
+          if (a[hvec[k]][wvec[l]] != b[k][l])
+          {
+            match = false;
+          }
+        }
+      }
+      
+      if(match) //見つかったらその時点で終了しちゃう
+      {
+        cout << "Yes" << endl;
+        return 0;
+      }
+    }
+  }
+
+  cout << "No" << endl;
   return 0;
 }
--- ABC/264/h.cpp
+++ ABC/264/h.cpp
@@ -10,11 +10,35 @@ using Graph = vector<vector<int>>;
 using mint = modint1000000007;
 
 int main() {
-  int n;
-  cin >> n;
+  int n = 3;
+  //cin >> n;
 
+  
+  //{0, 1, ..., n-1}の部分集合の全探索
+
+  // 各数字において、選ぶ/選ばないの2通りあるので
+  // 選び方は2^n -１通りある。
+  // 2^n は 1<<n　で表せるので bit < (1<<n) と書いておけば全探索できる。
+  for (int bit = 0; bit < (1 << n); bit++)
+  {
+    vector<int> S;
+    for (int i = 0; i < n; i++)  //各bitを捜査
+    {
+      if(bit & (1<<i)){ //列挙に0,1, ... ,nが含まれるか
+        S.push_back(i); //セットされているbitと対応する番号をSへ格納
+      }
+    }
+
+    cout << bit << ": {";
+    for (int i = 0; i < (int)S.size(); i++)
+    {
+      cout << S[i] << " ";
+    }
+    cout << bit << ": {";
+  
+  }
+  
   int ans = 0;
-
   cout << ans << endl;
   return 0;
 }
