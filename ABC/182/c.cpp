27218e05fb3ecbb8a334452b1bc5391b06789449
lVlnaga <cid15135@gmail.com>
2022-08-15 20:46:46 +0900

    modified:   ABC/182/c.cpp

--- ABC/182/c.cpp
+++ ABC/182/c.cpp
@@ -9,12 +9,71 @@ using P = pair<int,int>;
 using Graph = vector<vector<int>>;
 using mint = modint1000000007;
 
+/* 
+[]Nを入力
+[]bit全探索する(消す0, 残す1)
+  []残すbitの桁をvectorへpush_back
+  []消したbit数をおぼえておく
+  []残すbitから数字を復元
+  []3の倍数になるかチェック
+  []3の倍数になったらその時の消した桁数が最小化チェックして、最小なら保存。
+ */
+
+
 int main() {
-  int n;
+  string n;
+  ll min_k = LLONG_MAX;
   cin >> n;
 
-  int ans = 0;
+  //bit全探索
+  for (int bit = 0; bit < (1<<n.size()); bit++)
+  {
+    vector<int> v;
+    ll delete_digit=0;
+    for (int i = 0; i < (int)n.size(); i++)
+    {
+      if (bit & (1<<i))
+      {
+        v.push_back((int)(n[i]-'0'));//残す桁 charをintに変換
+      }
+      else
+      {
+        delete_digit++;//消す桁をカウント
+      }
+    }
 
-  cout << ans << endl;
+    if (delete_digit == (ll)n.size())
+    {
+      continue; //全消しはカウントしちゃだめ
+    }
+    
+
+    //数字を復元
+    ll number = 0;
+    for (int i = 0; i < (int)v.size(); i++)
+    {
+      number += v[i] * powl(10,i);
+    }
+    
+    //3の倍数になるかチェック
+    if((number % 3) == 0){
+      if (min_k > delete_digit)
+      {
+        min_k = delete_digit;
+      }
+    }
+  }
+  
+  
+  
+  if (min_k == LLONG_MAX)
+  {
+    cout << -1 << endl;
+  }
+  else
+  {
+    cout << min_k << endl;
+  }
+
   return 0;
 }
