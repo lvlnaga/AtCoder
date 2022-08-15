1e51afa412eae2bdf47b42a8b0108f53320ac256
lVlnaga <cid15135@gmail.com>
2022-08-15 11:55:33 +0900

    new file:   ABC/182/a.cpp 	new file:   ABC/182/b.cpp 	new file:   ABC/182/c.cpp 	new file:   ABC/182/d.cpp 	new file:   ABC/182/e.cpp 	new file:   ABC/182/f.cpp 	new file:   ABC/182/g.cpp 	new file:   ABC/182/h.cpp 	new file:   ABC/221/a.cpp 	new file:   ABC/221/b.cpp 	new file:   ABC/221/c.cpp 	new file:   ABC/221/d.cpp 	new file:   ABC/221/e.cpp 	new file:   ABC/221/f.cpp 	new file:   ABC/221/g.cpp 	new file:   ABC/221/h.cpp 	modified:   problem.exe 	modified:   problem.in

---
+++ ABC/221/c.cpp
@@ -0,0 +1,75 @@
+#include <bits/stdc++.h>
+using namespace std;
+#include <atcoder/all>
+using namespace atcoder;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep1(i, n) for (int i = 1; i < (int)(n+1); i++)
+using ll = long long;
+using P = pair<int,int>;
+using Graph = vector<vector<int>>;
+using mint = modint1000000007;
+
+int main() {
+/* 
+stringで取得する
+stringをint型に変換することはできる？
+→stoi関数でいける。(charからの変化ならatoi)。istringstreamでもいける。
+
+[x]string型で取得する
+[]bit全探索で分ける
+[]分けたパターンの中で一番でかい数字をそれぞれint型にする
+  []大きい数字から一桁目に入れていくみたいな？
+[]残りの一個が0になってたらcontinueする
+[]分けた数字を掛け算して maxとして残しておく。
+ */
+  int max = 0, seki = 0;
+  string n;
+  cin >> n;
+
+  //bit全探索
+  for (int bit = 0; bit < (1<<n.size()); bit++)
+  {
+    vector<int> A,B;
+    for (int i = 0; i < (int)n.size(); i++)
+    {
+      if (bit & (1<<i))
+      {
+        A.push_back((int)(n[i]-'0'));
+      }else
+      {
+        B.push_back((int)(n[i]-'0'));
+      }
+    }
+
+    //sortする
+    sort(A.begin(),A.end());
+    sort(B.begin(),B.end());
+
+    int numA =0;
+    int numB=0;
+
+    for (int i = 0; i < (int)A.size(); i++)
+    {
+      numA += A[i] * pow(10,i);
+    }
+    
+    for (int i = 0; i < (int)B.size(); i++)
+    {
+      numB += B[i] * pow(10,i);
+    }
+    
+    if (numA == 0 || numB == 0) continue;
+    
+    seki = numA * numB;
+
+    if (max < seki)
+    {
+      max = seki;
+    }
+  }
+  
+
+  cout << max << endl;
+  return 0;
+
+}
