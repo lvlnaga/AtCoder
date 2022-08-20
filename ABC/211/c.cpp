2e4a125e8efd0e9d99f5bf1db6160060c9a82a7d
lVlnaga <cid15135@gmail.com>
2022-08-20 22:10:09 +0900

    modified:   ABC/211/c.cpp

--- ABC/211/c.cpp
+++ ABC/211/c.cpp
@@ -10,11 +10,40 @@ using Graph = vector<vector<int>>;
 using mint = modint1000000007;
 
 int main() {
-  int n;
-  cin >> n;
+  string s,t;
+  cin >> s;
+  t = "chokudai";
+  const int mod = 1000000007;
+  //dp[i][j]
+  //「Sのi文字目までを使ってchokudaiのj文字目までを選択する方法」と定義する
+  // N=|S|, T=chokudai
+  //
+  vector<vector<ll>> dp(s.size()+1, vector<ll>(9,0));
 
-  int ans = 0;
-
-  cout << ans << endl;
+  for (ll i = 0; i < (ll)s.size()+1; i++)
+  {
+    for (ll j = 0; j < (ll)t.size()+1; j++)
+    {
+      if (j == 0)
+      {
+        dp[i][j] = 1;
+      }
+      else if (i == 0 && 1 <= j && j <= (ll)t.size())
+      {
+        dp[i][j] = 0;
+      }
+      else if (s[i-1]==t[j-1])
+      {
+        dp[i][j] = (dp[i-1][j-1] + dp[i-1][j])% mod;
+      }else
+      {
+        dp[i][j] = dp[i-1][j];
+      }
+    }
+  }
+  
+  //ll ans = dp[s.size()][t.size()];
+  
+  cout << dp[s.size()][t.size()] << endl;
   return 0;
 }
