3263e9163dc97331cf05ff86f0d2fa26502102bb
lVlnaga <cid15135@gmail.com>
2022-08-27 21:44:07 +0900

    modified:   AlgoShiki/Q3.cpp

--- AlgoShiki/Q3.cpp
+++ AlgoShiki/Q3.cpp
@@ -12,9 +12,16 @@ using mint = modint1000000007;
 int main() {
   int n;
   cin >> n;
+  vector<int> dp(n+1);
+  
+  dp[0] = 1; //登らないパターンは1通り
+  dp[1] = 1; //一段登るのは1通り
 
-  int ans = 0;
-
-  cout << ans << endl;
+  for (int i = 2; i <= n; i++)
+  {
+    dp[i] = dp[i-1] + dp[i-2];
+  }
+  
+  cout << dp[n] << endl;
   return 0;
 }
