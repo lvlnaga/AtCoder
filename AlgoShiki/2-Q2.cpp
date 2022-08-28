076b6f4aa3c7ccd0bb01b1519701058b2604107f
lVlnaga <cid15135@gmail.com>
2022-08-28 14:28:37 +0900

    modified:   AlgoShiki/2-Q2.cpp 	modified:   problem.exe 	modified:   problem.in

--- AlgoShiki/2-Q2.cpp
+++ AlgoShiki/2-Q2.cpp
@@ -13,7 +13,36 @@ int main() {
   int n;
   cin >> n;
 
-  int ans = 0;
+  vector<vector<int>> dp(n,vector<int>(n,0));
+
+  for (int i = 0; i < n; i++)
+  {
+    cin >> dp[0][i];
+  }
+  
+  for (int i = 1; i < n; i++)
+  {
+    for (int j = 0; j < n; j++)
+    {
+      if (j-1>= 0)
+      {
+        dp[i][j] += dp[i-1][j-1];
+        dp[i][j] %= 100;
+      }
+            
+      dp[i][j] += dp[i-1][j];
+      dp[i][j] %= 100;
+
+      if (j+1 < n)
+      {
+        dp[i][j] += dp[i-1][j+1];
+        dp[i][j] %= 100;
+      }
+    }
+  }
+  
+
+  int ans = dp[n-1][n-1];
 
   cout << ans << endl;
   return 0;
