4342bb5cf934bf292cf9ed27ccfab036099a4c6c
lVlnaga <cid15135@gmail.com>
2022-08-28 14:22:03 +0900

    modified:   AlgoShiki/2-Q1.cpp

--- AlgoShiki/2-Q1.cpp
+++ AlgoShiki/2-Q1.cpp
@@ -10,10 +10,36 @@ using Graph = vector<vector<int>>;
 using mint = modint1000000007;
 
 int main() {
-  int n;
-  cin >> n;
+  //int n;
+  //cin >> n;
 
-  int ans = 0;
+  vector<vector<int>> dp(4,vector<int>(4,0));
+
+  for (int i = 0; i < 4; i++)
+  {
+    cin >> dp[0][i];
+  }
+  
+  for (int i = 1; i < 4; i++)
+  {
+    for (int j = 0; j < 4; j++)
+    {
+      if (j-1>= 0)
+      {
+        dp[i][j] += dp[i-1][j-1];
+      }
+            
+      dp[i][j] += dp[i-1][j];
+      
+      if (j+1 < 4)
+      {
+        dp[i][j] += dp[i-1][j+1];
+      }
+    }
+  }
+  
+
+  int ans = dp[3][3];
 
   cout << ans << endl;
   return 0;
