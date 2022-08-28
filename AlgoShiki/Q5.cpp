d15aa6b77910cd0ec575b0bd77cf8a79e60a18cd
lVlnaga <cid15135@gmail.com>
2022-08-28 11:30:59 +0900

    modified:   AlgoShiki/Q5.cpp

--- AlgoShiki/Q5.cpp
+++ AlgoShiki/Q5.cpp
@@ -10,11 +10,33 @@ using Graph = vector<vector<int>>;
 using mint = modint1000000007;
 
 int main() {
-  int n;
-  cin >> n;
+  int n,m;
+  cin >> n >> m;
 
-  int ans = 0;
+  vector<int> a(n);
+  vector<int> dp(n,INT_MAX);
+  vector<int> tmp(m,INT_MAX);
+  vector<int> pretmp(m,INT_MAX);
 
-  cout << ans << endl;
+  for (int i = 0; i < n; i++)
+  {
+    cin >> a[i];
+  }
+
+  dp[0] = 0;
+
+
+  for (int i = 1; i < n; i++)
+  {
+    for (int j = 1; j <= m; j++)
+    {
+      if (j<=i)
+      {
+        dp[i] = min(dp[i], dp[i-j] + a[i]*j);
+      }
+    }
+  }
+
+  cout << dp[n-1] << endl;
   return 0;
 }
