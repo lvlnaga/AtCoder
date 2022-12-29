c4016b08ff27b76f68023534bab664266a71c7a4
lVlnaga <cid15135@gmail.com>
2022-12-29 14:37:59 +0900

    modified:   ABC/273/a.cpp 	modified:   problem.exe 	modified:   problem.in

--- ABC/273/a.cpp
+++ ABC/273/a.cpp
@@ -4,7 +4,7 @@
 #include <vector>
 #include <queue>
 #include <algorithm>
-#include <functional>  
+#include <functional>
 #include <set>
 #include <limits.h>
 #include <cmath>
@@ -13,33 +13,48 @@ using ll = long long;
 
 using namespace std;
 
-int kfunction(int k)
+/* 移動用関数 */
+//北へ移動(y--)
+int movN(int *x, int *y)
 {
-  if (k == 0)
-  {
-    return 1;
-  }
-  else
-  {
-    return k * kfunction(k - 1);
-  }
+  *y -= 1;
+  return 0;
+}
+//南へ移動(y++)
+int movS(int *x, int *y)
+{
+  *y += 1;
+  return 0;
+}
+//東へ移動(x++)
+int movE(int *x, int *y)
+{
+  *x += 1;
+  return 0;
+}
+//西へ移動(x--)
+int movW(int *x, int *y)
+{
+  *x -= 1;
+  return 0;
 }
 
+/* メイン関数 */
 int main()
 {
-  int n,m;
+  /* 入力の受け取り */
+  int n, m;
   cin >> n >> m;
 
   //地図の受け取り
-
-  //1行ずつstringのvectorに格納
+  // 1行ずつstringのvectorに格納
   vector<string> input_s(n);
   for (int i = 0; i < n; i++)
   {
     cin >> input_s[i];
   }
-  
-  //1文字ずつ stringの2次元vectorへ格納
+
+  // 1文字ずつ stringの2次元vectorへ格納
   vector<vector<string>> map(n, vector<string>(m));
   for (int i = 0; i < n; i++)
   {
@@ -48,17 +63,61 @@ int main()
       map[i][j] = input_s[i][j];
     }
   }
+
+  /* 移動処理 */
+  int now_x = 0, now_y = 0; //初期座標
+  string now_d;             //現在の床が示す方角
+  int ans_count = 0;        //移動距離を記憶
+  bool isFinished = false;  //終了条件
+
+  while (!isFinished)
+  {
+    now_d = map[now_y][now_x]; //現在の方角を取得
+
+    map[now_y][now_x] = "##"; //通過済みの座標は#でチェックを入れる
+
+    if (now_d == "N")
+    {
+      movN(&now_x, &now_y);
+    }
+    else if (now_d == "S")
+    {
+      movS(&now_x, &now_y);
+    }
+    else if (now_d == "E")
+    {
+      movE(&now_x, &now_y);
+    }
+    else if (now_d == "W")
+    {
+      movW(&now_x, &now_y);
+    }
+    else if (now_d == "##") //通過済みなのでここで終了
+    {
+      isFinished = true;
+      ans_count--; //通過済みのところへ移動した分はカウントしないため一つ戻す。
+    }
+    else // N,S,E,W以外の文字が登録されていた場合
+    {
+      cerr << "error: wrong direction is set" << endl;
+    }
+
+    ans_count++; //移動距離を加算
+
+    /* ゴールしたかどうか判定 */
+    // x,yがn,mからはみ出しているかチェックしてisFinishedを更新
+    if (now_x < 0 || m <= now_x) // xのチェック
+    {
+      isFinished = true;
+    }
+    if (now_y < 0 || n <= now_y) // yのチェック
+    {
+      isFinished = true;
+    }
+  }
   
-  //ここから操作
+  /* 答えの出力 */
+  cout << ans_count << endl;
 
-  
-
-
-
-  int ans = 0;
-
-
-
-  cout << ans << endl;
   return 0;
 }
