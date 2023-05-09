410443e98bb10b815419d055ec3fa14c24ceb0ac
lVlnaga <cid15135@gmail.com>
2023-05-09 23:12:30 +0900

    206 b

---
+++ ABC/206/b.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+#include <atcoder/all>
+using namespace atcoder;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep1(i, n) for (int i = 1; i < (int)(n+1); i++)
+using ll = long long;
+using P = pair<int,int>;
+using Graph = vector<vector<int>>;
+using mint = modint1000000007;
+
+int main() {
+  int n;
+  ll sum=0,day;
+  cin >> n;
+
+  for (ll i = 1; i < LONG_LONG_MAX; i++)
+  {
+    sum += i;
+    if(sum >= n)
+    {
+      day = i;
+      break;
+    }
+  }
+  
+  cout << day << endl;
+  return 0;
+}
